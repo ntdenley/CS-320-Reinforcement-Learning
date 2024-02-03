38332ab0ac12e7d02731dda2a3b33898924ddce1
wizardling1 <igork3210@gmail.com>
2024-02-02 16:16:29 -0800

    added a second gymnasium test

---
+++ Gymnasium/test2.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+env = gym.make("CartPole-v1", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
