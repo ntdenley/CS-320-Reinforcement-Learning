a6baa752960fb63f720582e6d6508984b1c515b0
gmbell30 <gmbell2000@gmail.com>
2024-04-12 18:26:17 -0700

    weekly pull 10

--- gui/guiProject.py
+++ gui/guiProject.py
@@ -1,187 +1,167 @@
-'''
-    Imports
-        - Gym (Gymnasium): OpenAI Environment Library
-        - Pygame: Multi Media Game Creation Library
-        - Numpy: Numerical Operation Library
-        - tkinter (tk, ttk): GUI Library
-        - Pillow: Rendering Library
-        - os: Operating System Library
-        - threading: Thread Library for running multiple processes
-'''
+"Imports"
 import tkinter as tk
+from tkinter import ttk
 import pygame
 import gym
 import os
 import threading
 import csv
-import numpy as np
-import matplotlib.pyplot as plt
-from tkinter import ttk
-from gymplots import aiPlot
 
-# Initialize pygame display
-pygame.init()
+"GUI Class: "
+class environmentGUI:
+    stepsDefault = 1000 # Initialize default maximum steps
 
-'''
-    GUI Class:
-        Initalizes and handles aspects of the main Tkinter GUI window and the OpenAI environments.
-'''
-class EnvironmentGUI:
-    '''
-        Init Function: constructor method for GUI environment and initalizes instance of the class with a Tkinter root window.
-            Parameters:
-                - self: instance of class object
-                - root: Tkinter root window
-                - outputPath: 
-    '''
+    "Init Function: constructor method for initializing the GUI environment"
     def __init__(self, root, outputPath):
         self.root = root
         self.outputPath = outputPath
         self.setupGUI()
         self.trainingData = []
         self.selectedID = None
-        self.stopTraining = False  # Flag to signal the training thread to stop
-        self.maxSteps = 1000  # Default value for max steps
-        self.sessionNumber = 1  # Initialize traning session counter
+        self.stopTraining = threading.Event()
+        self.maxSteps = self.stepsDefault
+        self.sessionNumber = 1
 
-    '''
-        setupGUI Function: Used to set up the graphical user interface (GUI) by providing the dimensions/title for the Tkinter root window. 
-            Parameters:
-                - self: instance of class object
-    '''
+    "setupGUI Function: method for setting up the GUI window with widgets (labels, button, boxes, etc.)"
     def setupGUI(self):
-        # Set title and dimensions for root window
+        # Set title and dimensions for the main root window
         self.root.title("CS-320-AI-GUI")
         self.root.geometry("800x600")
 
-        # Function call to create widgets and embed pygame display
-        self.createWidgets()
-        self.embedPygame()
-
-    '''
-        createWidgets Function: Used to create the different widgets (buttons, labels, dropdown menus, sliders, etc.) for the GUI. 
-            Parameters:
-                - self: instance of class object
-    '''
-    def createWidgets(self):
+        # 
         ttk.Label(self.root, text = "OpenAI Gym Environments:").grid(row = 0, column = 0, padx = 10, pady = 10, sticky = "w")
 
+        # Available gym environments the user can choose from
         self.environmentIDs = [
-            # Classic Control Environments
+            # Classic Control
             'Acrobot-v1', 'CartPole-v1', 'MountainCarContinuous-v0', 'MountainCar-v0', 'Pendulum-v1',
             
-            # Box2D Environments
+            # Box2D
             'LunarLander-v2', 'CarRacing-v2', 'BipedalWalker-v3',
             
-            # Toy Text Environments
+            # Toy Text
             'Blackjack-v1', 'Taxi-v3', 'CliffWalking-v0', 'FrozenLake-v1'
         ]
 
-        # Create Dropbox to show environments that the user can choose from
+        # Dropbox to display the available environments
         self.selectedEnvironment = tk.StringVar()
         self.environmentDropdown = ttk.Combobox(self.root, textvariable = self.selectedEnvironment, values = self.environmentIDs)
         self.environmentDropdown.grid(row = 0, column = 1, padx = 10, pady = 10, sticky = "w")
 
-        # Create entry box for user to adjust the maximum episode steps for training sessions
-        ttk.Label(self.root, text="Max Episode Steps:").grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "w")
+        # Entry box for the user to adjust the maximum episode steps 
+        ttk.Label(self.root, text = "Max Steps:").grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "w")
         self.maxEpisodeStepsEntry = ttk.Entry(self.root)
         self.maxEpisodeStepsEntry.grid(row = 1, column = 1, padx = 10, pady = 10, sticky = "w")
 
-        # Adjusts the buttons layout
+        # Frame to display buttons on the GUI window
         buttonFrame = tk.Frame(self.root)
         buttonFrame.grid(row = 0, column = 2, rowspan = 3, padx = 10, pady = 10, sticky = "ns")
 
-        # Create Buttons
-        ttk.Button(buttonFrame, text = "Load Environment", command = self.loadEnvironment).pack(fill = "x", padx = 5, pady = 5)
-        ttk.Button(buttonFrame, text ='Start Training Session', command = self.startTrainingSession).pack(fill = "x", padx = 5, pady = 5)
-        ttk.Button(buttonFrame, text = 'Quit', command = self.quitEnvironment).pack(fill = "x", padx = 5, pady = 5)
+        # Function call to create specific buttons
+        self.createButton(buttonFrame, "Load Environment", self.loadEnvironment, 5, 5)
+        self.createButton(buttonFrame, "Start", self.startTrainingSession, 5, 5)
+        self.createButton(buttonFrame, "Stop", self.stopTraining, 5, 5)
+        self.createButton(buttonFrame, "Quit", self.quitEnvironment, 5, 5)
 
-    '''
-        embedPygame Function: Used to embed pygame window into the GUI. 
-            Parameters:
-                - self: instance of class object
-    '''
+        # Function call to embed the pygame window into the GUI window
+        self.embedPygame()
+
+    " createButton Function: method for creating button widgets "
+    def createButton(self, parent, text, command, padx, pady):
+        button = ttk.Button(parent, text = text, command = command)
+        button.pack(fill = "x", padx = padx, pady = pady)
+        return button
+
+    " embedPygame Function: method for embedding the pygame display into the GUI window "
     def embedPygame(self):
-        # Check if the pygame display was initialized
+        # Check if pygame display was initialized
         if not pygame.display.get_init():
             pygame.display.init()
-        
-        # Create from to embed pygame display into
-        self.embedFrame = tk.Frame(self.root, width=600, height=500)
-        self.embedFrame.grid(row=3, column=0, columnspan=3, padx=(120, 10), pady=10)
-        
+
+        # Frame for embedding pygame display into
+        self.embedFrame = tk.Frame(self.root, width = 500, height = 500)
+        self.embedFrame.grid(row = 3, column = 0, columnspan = 3, padx = (120, 10), pady = 10)
+
         # Put pygame environment display into the frame
         os.environ['SDL_WINDOWID'] = str(self.embedFrame.winfo_id())
         os.environ['SDL_VIDEODRIVER'] = 'windib'
         self.screen = pygame.display.set_mode((800, 600))
 
-    '''
-        loadEnvironment Function: Used to embed the pygame screen into the Tkinter GUI frame
-            Parameters:
-                - self: instance of class object
-    '''
+    " loadEnvironment Function: method for loading the environment chosen by the user"
     def loadEnvironment(self):
-        # Load selected environment
+        # Close current environment if it already exists
+        if hasattr(self, 'environment') and self.environment:
+            self.environment.close() 
+
+        # Get chosen environment
         self.selectedID = self.selectedEnvironment.get()
 
         # Check if an environment wasn't selected
         if not self.selectedID:
-            self.showError("Choose Environment from Dropdown Menu")
+            self.showError("Choose an environment from the dropdown menu")
             return
-        
+
         # Check if the selected environment is valid
         if self.selectedID not in self.environmentIDs:
             self.showError("Choose a valid environment")
             return
 
-        # Check for errors when creating/loading the chosen environment
+        # Create environment and handle any errors
         try:
-            # Create environment
             self.environment = gym.make(self.selectedID, render_mode = "human")
+            self.resetEnvironment()
         except gym.error.Error as e:
-            # Handle errors
             self.showError(f"Error: Creating Environment: {e}")
 
-    '''
-        startTrainingSession Function: Used to 
-            Parameters:
-                - self: instance of class object
-    '''
-    def startTrainingSession(self):
-        # Check if an environment has been loaded
-        if not self.selectedID:
-            self.showError("Load an environment first")
-            return
-
-        # Function call to render the chosen environment for training session
-        self.renderEnvironment()
-
-    '''
-        renderEnvironment Function: Used to render the selected gym environment using pygame. 
-            Parameters:
-                - self: instance of class object
-    '''
-    def renderEnvironment(self):
+    " resetEnvironment Function: method for properly ressetting the environments before creating new ones "
+    def resetEnvironment(self):
         self.state = self.environment.reset()
         self.environment.render()
         self.done = False
         self.stepCount = 0
-        self.stopTraining = False  # Reset flag
-        self.maxSteps = int(self.maxEpisodeStepsEntry.get())  # Get user input for max episode steps
+
+    " startTrainingSession Function: method for starting a training session "
+    def startTrainingSession(self):
+        # Check if an environment has been loaded properly
+        if not self.selectedID:
+            self.showError("Need to load an environment")
+            return
+        
+        # Function call to initialize training session with specific settings
+        self.initializeTraining()
+
+    " initializeTraining Function: method for setting variables for training sessions "
+    def initializeTraining(self):
+        self.state = self.environment.reset()
+        self.environment.render()
+        self.done = False
+        self.stepCount = 0
+        self.stopTraining.clear()
+
+        # Get the maximum steps chosen by the user
+        maxEpisodeSteps = self.maxEpisodeStepsEntry.get()
+
+        # Set the maximum steps for training sessions
+        if maxEpisodeSteps.strip():
+            try:
+                self.maxSteps = int(maxEpisodeSteps)
+            except ValueError:
+                self.showError("Need to enter a valid integer")
+                return
+        else:
+            self.maxSteps = self.stepsDefault
+
+        # Starts a new thread for executing the training process
         self.trainingThread = threading.Thread(target = self.trainEnvironment)
         self.trainingThread.start()
 
-    '''
-        trainEnvironment Function: Used to train the environment (this uses random actions for now) until the episode ends or a maximum number of steps is met. 
-            Parameters:
-                - self: instance of class object
-    '''
+    " trainEnvironment Function: Used to train the environment (this uses random actions for now) "
     def trainEnvironment(self):
-        # Total reward from training session
+        # Total Reward 
         episodeReward = 0
 
-        # Train until maximum steps is reached, environment signals its done, or training session is stopped
-        while self.stepCount < self.maxSteps and not self.done and not self.stopTraining:
+        # Train until maximum steps is reached, environment is done, or training session stops
+        while self.stepCount < self.maxSteps and not self.done and not self.stopTraining.is_set():
             action = self.environment.action_space.sample()
             observation, reward, self.done, _, info = self.environment.step(action)
             episodeReward += reward
@@ -189,104 +169,92 @@ class EnvironmentGUI:
             pygame.display.flip()
             self.stepCount += 1
 
-        # Function call to save the data from training session into .csv file
-        self.saveTrainingData() 
+        # Function call to save metrics from training session
+        self.saveTrainingData()
 
-    '''
-        quitEnvironment Function: Used to quit the GUI besides closing the window. 
-            Parameters:
-                - self: instance of class object
-    '''
+    " stopTraining Function: method for pausing the training session once started"
+    def stopTraining(self):
+        self.stopTraining.set()
+
+    " quitEnvironment Function: Used to quit the GUI "
     def quitEnvironment(self):
         # Checks if environment attribute exists and closes GUI components
         if hasattr(self, 'environment') and self.environment:
-            self.stopTraining = True  
+            self.stopTraining.set()
             self.trainingThread.join()
-            self.environment.close()    
+            self.saveTrainingData()
+            with self.environment:
+                pass
         self.root.quit()
 
-    '''
-        saveTrainData Function: Used to save the data from training sessions into .csv files. 
-            Parameters:
-                - self: instance of class object
-    '''
+    " saveTrainData Function: Used to save the data from training sessions into .csv files "
     def saveTrainingData(self):
         # Get output path and construct path for output.csv files
-        outputPath = self.outputPath
-        outputFile = os.path.join(outputPath, f'training_data_session_{self.sessionNumber}.csv')
+        outputFile = os.path.join(self.outputPath, f'training_data_session_{self.sessionNumber}.csv')
+        fieldnames = ['Library', 'Environment', 'Reward', 'Step', 'Total Reward', 'Max Steps']
 
-        # Open csv file in write mode using a context manager
+         # Open csv file in write mode
         with open(outputFile, mode = 'w', newline = '') as file:
-            # Create csv writer object
-            writer = csv.writer(file)
-            
-            # Write column names
-            writer.writerow(['Library Name', 'Environment Name', 'Reward Per Step', 'Step', 'Total Reward', 'Total Steps', 'Max Steps'])
+            writer = csv.DictWriter(file, fieldnames = fieldnames)
+            writer.writeheader()
 
-            # Iterate over collected data collected
+            # Iterate over the collected data
             for data in self.trainingData:
-                # Get the gym environment library name
                 libraryName = self.getLibraryName(data[0])
-                
-                # Get environment name
                 environmentName = data[0]
-                
-                # Write data into rows
-                writer.writerow([libraryName, environmentName] + list(data[1:]))
+                rowData = {
+                    'Library': libraryName,
+                    'Environment': environmentName,
+                    'Reward': data[1],
+                    'Step': data[2],
+                    'Total Reward': data[3],
+                    'Max Steps': len(self.trainingData),
+                }
 
-        # Incrementsession counter and clear training data for new sessions
+                # Write data into rows
+                writer.writerow(rowData)
+
+        # Increment session counter and clear training data for new sessions
         self.sessionNumber += 1
         self.trainingData = []
 
-    '''
-        getLibraryName Function: Used to get the gym library name corresponding to environments. 
-            Parameters:
-                - self: instance of class object
-                - environmentID: the environment selected for training
-    '''
+    " getLibraryName Function: gets the gym library name corresponding to the selected environment used for training "
     def getLibraryName(self, environmentID):
-        if environmentID.startswith("Acrobot") or environmentID.startswith("CartPole") or environmentID.startswith("MountainCar") or environmentID.startswith("Pendulum"):
-            return "Classic Control"
-        elif environmentID.startswith("LunarLander") or environmentID.startswith("CarRacing") or environmentID.startswith("BipedalWalker"):
-            return "Box2D"
-        elif environmentID.startswith("Blackjack") or environmentID.startswith("Taxi") or environmentID.startswith("CliffWalking") or environmentID.startswith("FrozenLake"):
-            return "Toy Text"
-        else:
-            return "Unknown"
+        # Name of environment library correspinding to their gym environments
+        library = {
+            "Acrobot": "Classic Control",
+            "CartPole": "Classic Control",
+            "MountainCar": "Classic Control",
+            "Pendulum": "Classic Control",
+            "LunarLander": "Box2D",
+            "CarRacing": "Box2D",
+            "BipedalWalker": "Box2D",
+            "Blackjack": "Toy Text",
+            "Taxi": "Toy Text",
+            "CliffWalking": "Toy Text",
+            "FrozenLake": "Toy Text"
+        }
 
-    '''
-        showError Function: Used to display error messages in a pop up window to the user. 
-            Parameters:
-                - self: instance of class object
-                - message: error that will be displayed
-    '''
+        # Iterate over key-value pairs in library dictionary and return name
+        for prefix, libraryName in library.items():
+            if environmentID.startswith(prefix):
+                return libraryName
+        return "Unknown"
+
+    " showError Function: displays messages in the form of a pop up window to the user " 
     def showError(self, message):
-        # Create new top-level window
         errorWindow = tk.Toplevel(self.root)
-
-        # Create a title and label for error window
         errorWindow.title("Error")
         ttk.Label(errorWindow, text = message).pack(padx = 10, pady = 10)
-
-        # Button for closing the error window
         ttk.Button(errorWindow, text = "Close", command = errorWindow.destroy).pack(pady = 10)
 
-'''
-    Main Function
-'''
+" Main Function "
 def main():
-    # Create Tkinter winodw
     root = tk.Tk()
-
-    # Define output path and check if it exists. If not, create the directory
     outputPath = "output"
     if not os.path.exists(outputPath):
         os.makedirs(outputPath)
-
-    # Initialize GUI
-    application = EnvironmentGUI(root, outputPath)
-    
-    # Run Tkinter event loop
+    application = environmentGUI(root, outputPath)
     root.mainloop()
 
 if __name__ == "__main__":
